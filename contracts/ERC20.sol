b543f0c2d50c7e6bc3bc967d3e8f28ed28bc4eb1
Katherine Tsai <115205162+katherine84522@users.noreply.github.com>
2024-07-16 19:39:10 -0700

    Initialize

---
+++ contracts/ERC20.sol
@@ -0,0 +1,94 @@
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract ERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 0;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+
+    event Transfer(address from, address to, uint value);
+    event Approval(address owner, address spender, uint value);
+
+    constructor(string name_, string symbol_) {
+
+        name = name_;
+        symbol = symbol_;
+    }
+
+    function name() public returns(string){
+
+        return name;
+    }
+
+    function symbol() public returns(string){
+
+        return symbol;
+    }
+
+    function decimals() public returns (uint8){
+
+        return decimals;
+    }
+
+    function totalSupply() public returns(uint256){
+
+        return totalSupply;
+    }
+
+    function balanceOf(address account) public returns(uint256){
+
+        return balances[account];
+
+    }
+
+    function transfer(address payable to, uint256 value) external returns(bool){
+
+        require( to != address(0));
+        require( balances[msg.sender] >= value);
+
+        emit Transfer(msg.sender, to, value);
+        payable(msg.sender).transfer(to, value);// does it revert automatically if it fails?
+    }
+
+    function allowance(address owner, address spender) external returns(bool){
+
+        uint256 allowance = 0;
+
+        if(allowances[owner][spender] > 0){
+            allowance = allowances[owner][spender];
+        }
+
+        return allowance;
+    }
+
+    function approve(address spender, uint256 value) external returns(bool){
+
+        require( spender != address(0));
+
+        allowances[msg.caller][spender] = value;
+
+        emit Approval(msg.caller, spender, value);
+
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns(bool){
+
+        require( from != address(0));
+        require( to != address(0));
+        require( allowances[from][to] >= value);
+
+        emit Approval( from, to, value);
+
+        uint256 newValue = allowances[from][to] - value;
+        allowances[from][to] = newValue;
+
+        payable(from).transfer(to, value);
+    }
+
+}
